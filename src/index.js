2a6628b777ac4bff312922b86ef9d0e0a01d7e98
Kar Rui Lau <karrui.lau@gmail.com>
2019-10-08 00:46:40 +0800

    fix: app inaccessible when using link that is not '/' (#5)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from 'app';
 import AppProviders from 'contexts';
@@ -12,7 +13,9 @@ const render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <AppProviders>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </AppProviders>
     </Provider>,
     document.getElementById('root')
