6fb64965b58704a16c05e5d40e85beb23c382c13
Lau Kar Rui <karrui.lau@gmail.com>
2019-10-17 23:26:58 +0800

    feat(RoundDetails): add countdown timer

--- src/components/roundDetails/RoundDetails.jsx
+++ src/components/roundDetails/RoundDetails.jsx
@@ -1,36 +1,78 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Countdown, { zeroPad } from 'react-countdown-now';
 
 import './RoundDetails.scss';
 
+const countdownRenderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    return <NotifyDetails />;
+  }
+
+  return (
+    <div className="countdownDetails">
+      <div className="countdownDetails__header">Round Closing In:</div>
+      <div className="countdownDetails__timer">
+        <span className="countdownDetails__timer__counter">
+          <span className="countdownDetails__timer__counter__value">
+            {zeroPad(days)}
+          </span>
+          <span className="countdownDetails__timer__counter__text">days</span>
+        </span>
+        <span className="countdownDetails__timer__colon" />
+        <span className="countdownDetails__timer__counter">
+          <span className="countdownDetails__timer__counter__value">
+            {zeroPad(hours)}
+          </span>
+          <span className="countdownDetails__timer__counter__text">hours</span>
+        </span>
+        <span className="countdownDetails__timer__colon" />
+        <span className="countdownDetails__timer__counter">
+          <span className="countdownDetails__timer__counter__value">
+            {zeroPad(minutes)}
+          </span>
+          <span className="countdownDetails__timer__counter__text">
+            minutes
+          </span>
+        </span>
+        <span className="countdownDetails__timer__colon" />
+        <span className="countdownDetails__timer__counter">
+          <span className="countdownDetails__timer__counter__value">
+            {zeroPad(seconds)}
+          </span>
+          <span className="countdownDetails__timer__counter__text">
+            seconds
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
 const RoundDetails = () => {
   // TODO: Hook to API to retrieve next round.
-  // Should be null if no next round yet.
-  const roundDetails = null;
+  // Can be timestamp that has passed, then will automatically go to closed
+  const timeToEndOfRound = Date.now() + 10000;
 
   return (
     <div className="roundDetails">
       <div className="roundDetails__content">
-        {roundDetails ? <CountdownDetails /> : <NotifyDetails />}
+        <Countdown date={timeToEndOfRound} renderer={countdownRenderer} />
       </div>
       <Link to="/previous-round/summary">
-        View summary of the previous round
+        View summary of the previous round &gt;
       </Link>
     </div>
   );
 };
 
-const CountdownDetails = () => {
-  return <div>Round closing in</div>;
-};
-
 const NotifyDetails = () => {
   return (
-    <div className="notifyDetails">
-      <div className="notifyDetails__header">
+    <div className="countdownDetails">
+      <div className="countdownDetails__header">
         The next round has not opened yet.
       </div>
-      <div className="notifyDetails__text">
+      <div className="countdownDetails__text">
         You may still place your bids. We will notify you when the next round
         begins
       </div>
