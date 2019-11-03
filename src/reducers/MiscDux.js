b46c49e6ae7471460e00ea910c84d35b12a64f03
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-04 00:11:12 +0800

    feat: add user object into store to debounce backend calls

--- src/reducers/MiscDux.js
+++ src/reducers/MiscDux.js
@@ -6,15 +6,19 @@ import { BUYER } from 'constants/user';
 const misc = createSlice({
   name: 'misc',
   initialState: {
-    userType: BUYER
+    userType: BUYER,
+    user: null
   },
   reducers: {
     setUserType: (state, { payload }) => {
       state.userType = payload;
+    },
+    setUser: (state, { payload }) => {
+      state.user = payload;
     }
   }
 });
 
-export const { setUserType } = misc.actions;
+export const { setUserType, setUser } = misc.actions;
 
 export default misc.reducer;
