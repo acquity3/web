6de9fbc4654535478bca78ad600d315a86693f9c
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-04 10:23:18 +0800

    fix: chunk errors by retrying import with lazy loading

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,4 +17,32 @@ const addCommasToNumber = value => {
   return Number(value).toLocaleString('en-SG');
 };
 
-export { getInitials, getCurrentPathWithoutParam, addCommasToNumber };
+// https://dev.to/goenning/how-to-retry-when-react-lazy-fails-mb5
+const retryPromise = (promise, retriesLeft = 5, interval = 1000) => {
+  return new Promise((resolve, reject) => {
+    promise()
+      .then(resolve)
+      .catch(error => {
+        setTimeout(() => {
+          if (retriesLeft === 1) {
+            // reject('maximum retries exceeded');
+            reject(error);
+            return;
+          }
+
+          // Passing on "reject" is the important part
+          retryPromise(promise, retriesLeft - 1, interval).then(
+            resolve,
+            reject
+          );
+        }, interval);
+      });
+  });
+};
+
+export {
+  getInitials,
+  getCurrentPathWithoutParam,
+  addCommasToNumber,
+  retryPromise
+};
